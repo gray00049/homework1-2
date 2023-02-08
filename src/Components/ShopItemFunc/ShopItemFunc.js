26253924037ce1f0400e4da550978e3b8e509461
gray00049 <gray00049@bk.ru>
2023-02-08 21:58:56 +0300

    final version

---
+++ src/Components/ShopItemFunc/ShopItemFunc.js
@@ -0,0 +1,37 @@
+import PropTypes from "prop-types";
+
+function ShopItemFunc(props) {
+  return (
+    <div className="main-content">
+      <h2>{props.item.brand}</h2>
+      <h1>{props.item.title}</h1>
+      <h3>{props.item.description}</h3>
+      <div className="description">{props.item.descriptionFull}</div>
+      <div className="highlight-window mobile">
+        <div className="highlight-overlay"></div>
+      </div>
+      <div className="divider"></div>
+      <div className="purchase-info">
+        <div className="price">
+          {props.item.currency}
+          {props.item.price.toFixed(2)}
+        </div>
+        <button>Добавить в корзину</button>
+      </div>
+    </div>
+  );
+}
+
+ShopItemFunc.propTypes = {
+  item: PropTypes.object,
+  item: PropTypes.shape({
+    brand: PropTypes.string,
+    title: PropTypes.string,
+    description: PropTypes.string,
+    descriptionFull: PropTypes.string,
+    price: PropTypes.number,
+    currency: PropTypes.string
+  })
+};
+
+export default ShopItemFunc;
